5c16c58ffcdec8303501fb038d28b751afbb6f06
10nesee <98235697+10nesee@users.noreply.github.com>
2024-09-24 15:22:50 -0500

    Create parse options and command parameter

--- oss.c
+++ oss.c
@@ -8,7 +8,47 @@
 #define CLOCK_SIZE sizeof(int)*2 //
 
 
-int main(int argc, char *arggv[]) {
+
+int main(int argc, char *argv[]) {
+    // Command-line arguments and their defaults
+    int proc = 1;  // Default number of processes
+    int simul = 2; // Default number of simultaneous workers
+    int timelimit = 3; // Default time limit for children
+    int interval = 50;  // Default interval in milliseconds
+
+    // Parse arguments using getopt
+    int opt;
+    while ((opt = getopt(argc, argv, "hn:s:t:i:")) != -1) {
+        switch (opt) {
+            case 'h':
+                printf("Usage: %s [-h] [-n proc] [-s simul] [-t timelimit] [-i interval]\n", argv[0]);
+                exit(0);
+            case 'n':
+                proc = atoi(optarg);
+                break;
+            case 's':
+                simul = atoi(optarg);
+                break;
+            case 't':
+                timelimit = atoi(optarg);
+                break;
+            case 'i':
+                interval = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-h] [-n proc] [-s simul] [-t timelimit] [-i interval]\n", argv[0]);
+                exit(1);
+        }
+    }
+
+    // Print the parsed values 
+    printf("Number of processes: %d\n", proc);
+    printf("Number of simultaneous workers: %d\n", simul);
+    printf("Time limit for children: %d\n", timelimit);
+    printf("Interval to launch children (ms): %d\n", interval);
+
+
+
 
 // Create shared memory
 int shmid = shmget(SHMKEY, 2 * sizeof(int), 0777 | IPC_CREAT);
